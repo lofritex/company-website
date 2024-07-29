17ffe6acf4baa655e67ca61e805b633d7c064c40
ShibinRasheed <shibinrasheed333@gmail.com>
2024-07-29 21:07:47 +0530

    V Carousel Done

--- src/Pages/Components/Carousel.jsx
+++ src/Pages/Components/Carousel.jsx
@@ -5,10 +5,6 @@ import Arrow from "../Images/Arrow.svg";
 export default function Carousel({ data }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const carouselStyle = {
-    transform: `translateX(-${currentIndex * 100}%)`,
-  };
-
   const nextSlide = () => {
     return setCurrentIndex((prevIndex) => {
       if (prevIndex == 5) return 5;
@@ -24,17 +20,17 @@ export default function Carousel({ data }) {
   };
 
   return (
-    <div className="flex h-[400px] items-center">
+    <div className="flex h-screen items-center">
       <button className="rotate-180 w-40 h-10" onClick={prevSlide}>
       <img src={Arrow} alt="Left Arrow" className="w-16 h-10"/>
       </button>
-      <div id="carousel-container" className="flex gap-2 overflow-hidden ">
+      <div id="carousel-container" className="flex items-center gap-2 overflow-hidden h-full  ">
         {data.map((card,index) => {
           return (
             <div
               key={card.title}
-              className={`w-1/5 h-full shrink-0 transition-transform duration-700 ease-in-out flex justify-center`}
-              style={{ transform : `translateX(-${currentIndex * 100}%) translateY(${(((-1)**(currentIndex+1))*(-1)**index) *100}px)`}}
+              className={`w-1/5 h-full shrink-0 transition-transform duration-700 ease-in-out flex justify-center items-center`}
+              style={{ transform : `translateX(-${currentIndex * 100}%) translateY(${index == currentIndex+2 ? (((-1)**(currentIndex))*(-1)**index) *70 : (((-1)**(currentIndex+1))*(-1)**index) *30 }px)`}}
             >
               <Cards card={card} />
             </div>
@@ -48,3 +44,7 @@ export default function Carousel({ data }) {
     </div>
   );
 }
+
+
+
+
