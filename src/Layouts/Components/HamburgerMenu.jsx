1f82f125b6e1d0474ec952d7a92201744444790e
ShibinRasheed <shibinrasheed333@gmail.com>
2024-08-06 12:53:41 +0530

    Add Drop shadow to hamburger Menu

--- src/Layouts/Components/HamburgerMenu.jsx
+++ src/Layouts/Components/HamburgerMenu.jsx
@@ -14,31 +14,40 @@ export default function HamburgerMenu() {
     <>
       <button
         onClick={toggler}
-        className="flex h-nav w-full justify-end transition-all duration-500 ease-in-out absolute top-0 right-0 z-30 p-4"
+        className="absolute right-0 top-0 z-30 flex h-nav w-full justify-end p-4 transition-all duration-500 ease-in-out"
       >
         <img
           src={menuActive ? closeIcon : hamburgerIcon}
           alt={menuActive ? "Close Menu" : "Open Menu"}
-          className="w-8 h-8"
+          className="h-8 w-8"
         />
       </button>
 
       <div
-        className={`fixed right-0 top-0 min-h-[400px] w-full max-w-[80%] bg-white transition-transform duration-500 ease-in-out transform ${
+        className={`fixed right-0 top-0 min-h-[400px] w-full max-w-[80%] transform border border-gray-300 bg-white shadow-lg transition-transform duration-500 ease-in-out ${
           menuActive ? "translate-x-0" : "translate-x-full"
         } z-20`}
       >
         <nav className="flex flex-col gap-6 p-10 text-xl">
-          <Link to="/" className="hover:text-blue-500 transition-colors duration-200">
+          <Link
+            to="/"
+            className="transition-colors duration-200 hover:text-blue-500"
+          >
             Home
           </Link>
-          <Link to="/services" className="hover:text-blue-500 transition-colors duration-200">
+          <Link
+            to="/services"
+            className="transition-colors duration-200 hover:text-blue-500"
+          >
             Services
           </Link>
-          <Link to="/aboutus" className="hover:text-blue-500 transition-colors duration-200">
+          <Link
+            to="/aboutus"
+            className="transition-colors duration-200 hover:text-blue-500"
+          >
             About us
           </Link>
-          <button className="items-center justify-center rounded-md bg-gradient-to-tr from-[#1A05FF] to-[#6FCFFF] px-6 py-2 text-white lg:flex hover:bg-gradient-to-tl transition-all duration-300">
+          <button className="items-center justify-center rounded-md bg-gradient-to-tr from-[#1A05FF] to-[#6FCFFF] px-6 py-2 text-white transition-all duration-300 hover:bg-gradient-to-tl lg:flex">
             Get in touch
           </button>
         </nav>
