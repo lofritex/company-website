4e8ba28c8fce980b917ad631cb2298ea820d8d9d
ShibinRasheed <shibinrasheed333@gmail.com>
2024-08-09 22:35:07 +0530

    Add Services Cards

--- src/Pages/Components/ServicesCard.jsx
+++ src/Pages/Components/ServicesCard.jsx
@@ -1,21 +1,26 @@
+import { NavLink as Link } from "react-router-dom";
+import LearnMoreArrow from "../Images/LearnMoreArrow.svg";
 import WebDevImage from "../Images/WebDevImage.svg";
 
 export default function ServicesCard() {
   return (
-    <div className="m-5 flex max-w-[400px] flex-col items-center justify-center bg-[#5CA1FF] p-5  text-white">
+    <div className="m-5 flex max-h-[500px] max-w-[350px] flex-col items-center justify-center rounded-xl bg-[#5CA1FF] p-5 text-white">
       <img
         src={WebDevImage}
         alt="Web Developer Graphical"
-        className="min-w-12 max-w-40"
+        className="m-4 min-w-12 max-w-40"
       />
-      <h2>Web Development</h2>
-      <p>
+      <h2 className="p-5 text-3xl font-bold">Web Development</h2>
+      <p className="px-7 pb-3 text-center font-thin">
         With lots of unique blocks, you can easily build a page without coding.
         Build your next landing page.
       </p>
+      <Link to="/">
+        <div className="flex">
+          <h6 className="p-5 text-xl font-bold">Learn More</h6>
+          <img src={LearnMoreArrow} alt="" />
+        </div>
+      </Link>
     </div>
   );
 }
-///* Graphic Design */
-
-
