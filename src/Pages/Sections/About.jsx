1e5572847773c1bdf404dcb2bf9556a61272e1cd
ShibinRasheed <shibinrasheed333@gmail.com>
2024-08-09 21:56:49 +0530

    Change Contents of About us Page

--- src/Pages/Sections/About.jsx
+++ src/Pages/Sections/About.jsx
@@ -4,27 +4,28 @@ export default function About() {
   return (
     <section className="relative flex w-full flex-col items-center justify-center">
       <h1 className="p-10 text-6xl font-bold">ABOUT US</h1>
-      <div className="flex items-center justify-center flex-col lg:flex-row">
+      <div className="flex flex-col items-center justify-center lg:flex-row">
         <div className="flex w-half items-center justify-center">
           <img src={AboutPic} alt="About pic" className="h-6/7" />
         </div>
-        <div className="lg:w-half lg:p-4">
-          <p className="px-10">
+        <div className="flex flex-col items-center justify-center lg:w-half lg:p-4 relative">
+          <p className="max-w-[600px] px-10">
             Welcome to Lofritex, where our commitment to Loyalty, Friendship,
             Technology, and Expertise defines everything we do. Our name stands
             as a testament to these core values, guiding us in every aspect of
             our business.
-            <br/><br/>
-            At Lofritex, we are driven by our loyalty towards our
-            clients and technology. We believe in bringing our technological
-            expertise to every project, ensuring that we deliver innovative
-            solutions that meet the unique needs of our clients. Our dedicated
-            team of professionals works tirelessly to build strong, lasting
+            <br />
+            <br />
+            At Lofritex, we are driven by our loyalty towards our clients and
+            technology. We believe in bringing our technological expertise to
+            every project, ensuring that we deliver innovative solutions that
+            meet the unique needs of our clients. Our dedicated team of
+            professionals works tirelessly to build strong, lasting
             relationships with our partners, clients, and the community.
           </p>
+          <div className="absolute bottom-0 right-10  h-4 w-32 bg-blue-500"></div>
         </div>
       </div>
-      <div className="absolute bottom-36 right-20 h-4 w-32 bg-blue-500"></div>
     </section>
   );
 }
--- src/Pages/Sections/Misson.jsx
+++ src/Pages/Sections/Misson.jsx
@@ -6,31 +6,25 @@ export default function Misson() {
     <section className=" flex w-full items-center justify-center py-40">
       <div className="flex w-half flex-col items-center justify-center">
         <img src={mission} alt="Our Mission Pic" className="m-10 w-40" />
-        <h2>OUR MISSION</h2>
+        <h2 className="font-bold text-3xl">OUR MISSION</h2>
         <div className="flex">
           <div className="mt-10 h-[1px] w-40 bg-blue-600"></div>
           <div className="ml-2 mt-10 h-[1px] w-40 bg-blue-600"></div>
         </div>
-        <p className="w-1/2 p-10"> 
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque
-          blanditiis autem quia eius id, corporis quos nam laborum beatae et
-          magnam fuga dignissimos facilis ut aliquid. Minus, exercitationem
-          consectetur!
+        <p className="w-1/2 px-20 pt-10 pb-20"> 
+        We aim to create an ecosystem where technological solutions are one call away.
         </p>
       </div>
       <div className="h-44 w-[1px] bg-black"></div>
       <div className="flex w-half flex-col items-center">
         <img src={vision} alt="Our Vision Pic" className="m-10 w-40" />
-        <h2>OUR VISION</h2>
+        <h2 className="font-bold text-3xl">OUR VISION</h2>
         <div className="flex">
           <div className="mt-10 h-[1px] w-40 bg-blue-600"></div>
           <div className="ml-2 mt-10 h-[1px] w-40 bg-blue-600"></div>
         </div>
-        <p className="w-1/2 p-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cumque
-          blanditiis autem quia eius id, corporis quos nam laborum beatae et
-          magnam fuga dignissimos facilis ut aliquid. Minus, exercitationem
-          consectetur!
+        <p className="w-1/2 px-20 py-10">
+        To create an ecosystem where technological solutions are always within reach, empowering individuals and businesses to thrive with ease and efficiency.
         </p>
       </div>
     </section>
